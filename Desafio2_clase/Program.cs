037b65d70348614fff938d6b38523b20154cdad9
XavierR007 <creyes4@miumg.edu.gt>
2023-03-29 08:59:42 -0600

    ejer

---
+++ Desafio2_clase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Desafio2_clase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
